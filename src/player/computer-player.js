80d450983a0abde6a9484b91953bf86eddc4a4ce
BinoManjesh <binomanjesh@gmail.com>
2024-01-31 15:08:57 +0530

    Improve computer player strategy

--- src/player/computer-player.js
+++ src/player/computer-player.js
@@ -3,28 +3,48 @@ import Player from "./player";
 export default class ComputerPlayer extends Player {
   setTurn() {
     super.setTurn();
-    let numValid = 0;
     const board = this.enemyGameboard.board;
     const size = board.length;
-    for (let i = 0; i < size; ++i) {
-      for (let j = 0; j < size; ++j) {
-        if (!board[i][j].isHit) {
-          ++numValid;
+    let seenSpecial = false;
+    const isSpecial = function (x, y) {
+      for (const [i, j] of [
+        [x + 1, y],
+        [x - 1, y],
+        [x, y + 1],
+        [x, y - 1],
+      ]) {
+        if (board[i] == undefined || board[i][j] == undefined) {
+          continue;
+        }
+        const cell = board[i][j];
+        if (cell.isHit && cell.ship != null) {
+          return true;
         }
       }
-    }
-    const chosenMove = Math.floor(Math.random() * numValid);
-    let numMove = 0;
+      return false;
+    };
+    let choices = [];
     for (let i = 0; i < size; ++i) {
       for (let j = 0; j < size; ++j) {
+        const special = isSpecial(i, j);
         if (!board[i][j].isHit) {
-          if (numMove == chosenMove) {
-            this.makeMove(i, j);
-            return;
+          if (seenSpecial) {
+            if (special) {
+              choices.push([i, j]);
+            }
+          } else {
+            if (special) {
+              seenSpecial = true;
+              choices = [[i, j]];
+            } else {
+              choices.push([i, j]);
+            }
           }
-          ++numMove;
         }
       }
     }
+    console.log(choices);
+    const move = choices[Math.floor(Math.random() * choices.length)];
+    this.makeMove(...move);
   }
 }
